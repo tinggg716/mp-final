9429b73eef455c4a286ffa119c17c47410d92925
tinggg716 <chialiting716@gmail.com>
2024-11-21 10:57:26 +0800

    Update dast_chatbot_finalv2.py

--- dast_chatbot_finalv2.py
+++ dast_chatbot_finalv2.py
@@ -11,6 +11,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+import streamlit as st
 
 from openai import OpenAI
 api_key = st.secrets["OPENAI_API_KEY"]
